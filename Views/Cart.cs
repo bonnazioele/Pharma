049e7a422eba401203eea59bd7c3791bad57b4bb
Christian Javidil A. Sy <christianjavidil.sy.22@usjr.edu.ph>
2024-12-06 14:25:52 +0800

    Add files via upload

---
+++ Views/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharma.Models
+{
+    public class Cart
+    {
+        [Key]
+        required
+        public int prodId { get; set; }
+        public string prodName { get; set; }
+        public string prodDescription { get; set; } 
+        public int qnty { get; set; }
+        public int add {  get; set; }
+    }
+}
---
+++ Views/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Pharma.Models
+{
+    public class Login
+    {
+        [Key] public int userID {  get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
